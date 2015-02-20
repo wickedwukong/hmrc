b0db2c7c8209de389daf09ea21e65bac66cfb2f9
Xuemin Guan <gxuemin@gmail.com>
2015-02-20 21:28:23 +0000

    Making a second simple case passing with chaning as little code as possible

--- src/main/scala/hmrc/Checkout.scala
+++ src/main/scala/hmrc/Checkout.scala
@@ -1,5 +1,10 @@
 package hmrc
 
 class Checkout {
-  def scan(shoppingCart: List[String]): Int = 0
+  def scan(shoppingCart: List[String]): Int = {
+    shoppingCart match {
+      case Nil => 0
+      case _ => 60
+    }
+  }
 }
--- src/test/scala/hmrc/CheckoutSpec.scala
+++ src/test/scala/hmrc/CheckoutSpec.scala
@@ -6,8 +6,17 @@ class CheckoutSpec extends Specification {
 
   "Empty shopping cart" should {
     "cost zero" in {
-      val emptyShoppingList: List[String] = List()
-      new Checkout().scan(emptyShoppingList) must_== 0
+      val emptyShoppingCart: List[String] = List()
+      new Checkout().scan(emptyShoppingCart) must_== 0
     }
   }
+
+  "An apple" should {
+    "cost 60p" in {
+      val oneAppleOnlyShoppingCart: List[String] = List("apple")
+      new Checkout().scan(oneAppleOnlyShoppingCart) must_== 60
+    }
+  }
+
+
 }
