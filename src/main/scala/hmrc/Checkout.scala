d77c45aa471da512d7c4dc0356ebd4d583d45a2b
Xuemin Guan <gxuemin@gmail.com>
2015-02-20 23:02:01 +0000

    a bit of refactoring to make the apple offer logic independent

--- src/main/scala/hmrc/Checkout.scala
+++ src/main/scala/hmrc/Checkout.scala
@@ -2,15 +2,22 @@ package hmrc
 
 import scala.collection.immutable.Iterable
 
+object Checkout {
+  def applyAppleOffer(numberOfApples: Int): Int = ((numberOfApples / 2) + (numberOfApples % 2)) * 60
+}
+
 class Checkout {
+
+  import Checkout._
+
   def scan(shoppingCart: List[String]): Int = {
 
     val costAfterOfferApplied: Iterable[Int] = shoppingCart.groupBy(item => item).map { case (item, itemList) => {
       if (item == "apple")
-        ((itemList.size / 2) + (itemList.size % 2)) * 60
+        applyAppleOffer(itemList.size)
       else
         itemList.size * 25
-     }
+    }
     }
 
     costAfterOfferApplied.foldLeft(0)((acc, singleItemCost) => acc + singleItemCost)
